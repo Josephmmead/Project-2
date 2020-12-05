13ba8b5932866d79c6d908ae875d8a39ca1fc5ed
Chris Carman <ccarmander@gmail.com>
2020-12-05 11:18:25 -0500

    pushing up frontend2

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,6 @@
 CREATE DATABASE main_db;
 USE main_db;
 
-CREATE TABLE 'Thread'(
-    `id` int (11) NOT NULL AUTO_INCREMENT,
-    `thread_name` VARCHAR(255) NOT NULL,
-    `user` varchar(255) NOT NULL,
-    `body` TEXT NOT NULL,
-    
-    PRIMARY KEY (`id`)
-);
+
 
     
