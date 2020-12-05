ac5a794ef6add67e543b9cb4a6da5d5874f05d24
Joseph Mead <josephmmead@gmail.com>
2020-12-05 14:35:43 -0500

    changed controller to routes and added an additional model

--- models/Thread.js
+++ models/Thread.js
@@ -1,23 +1,24 @@
 module.exports = function(sequelize, DataTypes) {
     var Thread = sequelize.define("Thread", {
-      thread_name: {
+      title: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
           len: [1]
         }
       },
-      user: {
-        type: DataTypes.STRING,
-        defultValue: "Anonymous"
-      },
-      body: {
-        type: DataTypes.TEXT,
-      },
       category: {
         type: DataTypes.STRING,
         defaultValue: "Gaming"
       }
     });
+
+    Thread.associate = function(models) {
+      // Associating Author with Posts
+      // When an Author is deleted, also delete any associated Posts
+      Thread.hasMany(models.Post, {
+        onDelete: "cascade"
+      });
+    };
     return Thread;
   };
